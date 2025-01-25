8f1db91e31af5c17a256a783d79b2e40fc7ef82b
Tumanggor <mendienta990@gmail.com>
2025-01-25 13:34:02 +0700

    Auto-commit: Updates made on Sat Jan 25 01:34:02 PM WIB 2025

--- domain/character.py
+++ domain/character.py
@@ -1,14 +1,24 @@
 class Character:
-    def __init__(self, name, species, age, hunger_level=0, happiness_level=100):
+    def __init__(self, name, species, age, hunger_level=0, happiness_level=100, energy_level=100):
         self.name = name
         self.species = species
         self.age = age
         self.hunger_level = hunger_level
         self.happiness_level = happiness_level
+        self.energy_level = energy_level
 
-    def feed(self, amount):
-        """
-        Feeds the character, decreasing the hunger level.
-        """
-        self.hunger_level = max(0, self.hunger_level - amount)
-        self.happiness_level = min(100, self
+    def feed(self, food_amount):
+        self.hunger_level -= food_amount
+        if self.hunger_level < 0:
+            self.hunger_level = 0
+        self.happiness_level += 10
+        self.energy_level += 5
+
+    def play(self, play_time):
+        self.happiness_level += play_time * 5
+        self.energy_level -= play_time * 10
+
+    def rest(self, rest_time):
+        self.energy_level += rest_time * 20
+        self.hunger_level += rest_time * 2
+        self.happiness_level += rest_time * 3
