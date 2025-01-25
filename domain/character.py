cf393c954ea9f9ce7f696650170900a7cdf3c54a
danyswr <daniswara.173@gmail.com>
2025-01-25 13:44:48 +0700

    mwmwmwmk

--- domain/character.py
+++ domain/character.py
@@ -1,14 +1,16 @@
 class Character:
-    def __init__(self, name, species, age, hunger_level=0, happiness_level=100):
+    def __init__(self, name, health, hunger_level=0, happiness_level=100):
         self.name = name
-        self.species = species
-        self.age = age
+        self.health_level = health
         self.hunger_level = hunger_level
         self.happiness_level = happiness_level
 
     def feed(self, amount):
-        """
-        Feeds the character, decreasing the hunger level.
-        """
+        # Mengurangi hunger_level sesuai dengan jumlah yang diberikan, memastikan tidak kurang dari 0
         self.hunger_level = max(0, self.hunger_level - amount)
-        self.happiness_level = min(100, self
+        
+        # Meningkatkan happiness_level, tetapi tidak melebihi 100
+        self.happiness_level = min(100, self.happiness_level + amount)
+
+        # Menampilkan status setelah diberi makan
+        print(f"{self.name} has been fed! Hunger level: {self.hunger_level}, Happiness level: {self.happiness_level}")
