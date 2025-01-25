24f4bcff431ffdc6bd467a51c99820852b8aa32b
danyswr <daniswara.173@gmail.com>
2025-01-25 13:15:55 +0700

    mwemwek

--- application/game_setup.py
+++ application/game_setup.py
@@ -1,15 +1,14 @@
 from tkinter import simpledialog, messagebox
-from domain.character import Character
+from domain.character import Character  
 
 def setup_game():
-    player_name = simpledialog.askstring("Input", "Enter your hero's name:")
-    enemy_name = simpledialog.askstring("Input", "Enter the enemy's name:")
+    Pet_name = simpledialog.askstring("Input", "Enter your Pet's name:")
 
-    if not player_name or not enemy_name:
-        messagebox.showerror("Error", "Both names must be entered!")
-        return None, None
+    if Pet_name:
+        pet = Character(Pet_name)
+        messagebox.showinfo("Pet Created", f"Your pet, {pet.name}, has been created!")
+    else:
+        messagebox.showerror("Error", "You must enter a name for your pet!")
+        return None
 
-    player = Character(player_name)
-    enemy = Character(enemy_name)
-
-    return player, enemy
+    return pet
